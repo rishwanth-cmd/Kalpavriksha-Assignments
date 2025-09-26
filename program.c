67136962cf9384b38e43b698976080829f807530
rishwanthk <iamrishwanthk@gmail.com>
2025-09-26 10:46:10 +0530

    implemented the logic

--- program.c
+++ program.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
+int evaluate_helper(int a, int b, char op);
 int evaluate_expression(const char* expression);
+int precedence_checker(char op);
+
 
 int main()
 {
@@ -10,7 +13,9 @@ int main()
     fgets(expression, sizeof(expression), stdin);    // getting the expression from user
     expression[strlen(expression)-1]='\0';   // removing the newline character
     int result = evaluate_expression(expression);   // calling the function to evaluate the expression
+    printf("%d",result);
     return 0;
+
 }
 
 int evaluate_expression(const char* expression)
@@ -23,6 +28,12 @@ int evaluate_expression(const char* expression)
     while(i<len)
     {
         char ch = expression[i];
+
+        if(ch==' ')
+        {
+            i++;
+            continue;
+        }
         
         // converting string to integer and pushing it to operands stack
         if(ch>='0' && ch<='9')
@@ -37,18 +48,70 @@ int evaluate_expression(const char* expression)
         }
         else if(ch=='+' || ch=='-' || ch=='*' || ch=='/')
         {
+            while(operators_top>=0 && precedence_checker(operators[operators_top])>=precedence_checker(ch))
+            {
+                int b = operands[operands_top--];
+                int a = operands[operands_top--];
+                char op = operators[operators_top--];
+                int res = evaluate_helper(a,b,op);
+                operands[++operands_top] = res;
+            }
+
             // pushing operator to operators stack
             operators[++operators_top] = ch;
             i++;
         }
         else
         {
-            i++;   
+            printf("Invalid Expression");
+            return 0;
         }
     }
-    for(int j=0; j<=operands_top; j++)
-        printf("%d ", operands[j]);
-    for(int j=0; j<=operators_top; j++)
-        printf("%c ", operators[j]);
+
+    while(operators_top>=0)
+    {
+        int b = operands[operands_top--];
+        int a = operands[operands_top--];
+        char op = operators[operators_top--];
+        int res = evaluate_helper(a, b, op);
+        operands[++operands_top] = res;
+    }
+
+    return operands[operands_top];
+    // for(int j=0; j<=operands_top; j++)
+    //     printf("%d ", operands[j]);
+    // for(int j=0; j<=operators_top; j++)
+    //     printf("%c ", operators[j]);
+    // return 0;
+}
+
+int precedence_checker(char op)
+{
+    if(op == '/' || op == '*')
+    {
+        return 2;
+    }
+    if(op == '+' || op == '-')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int evaluate_helper(int a, int b, char op)
+{
+    switch (op)
+    {
+        case '+':
+            return a+b;
+        case '-':
+            return a-b;
+        case '*':
+            return a*b;
+        case '/':
+            return a/b;
+        default:
+            break;
+    }
     return 0;
 }
