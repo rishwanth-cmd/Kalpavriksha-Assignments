59756aef8b290d963438d99bf6a91a33b3bb0cac
rishwanthk <iamrishwanthk@gmail.com>
2025-10-19 11:29:35 +0530

    implemented function for rotation and smoothing

--- imageProcessing.c
+++ imageProcessing.c
@@ -3,6 +3,7 @@
 
 void rotate90Degree(int **matrix, int n);
 void displayMatrix(int **matrix, int n);
+void applySmoothing(int **matrix, int n);
 
 int main()
 {
@@ -27,6 +28,9 @@ int main()
     displayMatrix(matrix, n);
     rotate90Degree(matrix, n);
     displayMatrix(matrix, n);
+    applySmoothing(matrix, n);
+    displayMatrix(matrix, n);
+
 
 }
 
@@ -43,6 +47,16 @@ void rotate90Degree(int **matrix, int n)
             
         }
     }
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n/2; j++)
+        {
+            int temp = *(*(matrix + i) + j);
+            *(*(matrix + i) + j) = *(*(matrix + i) + (n - 1 - j));
+            *(*(matrix + i) + (n - 1 - j)) = temp;
+        }
+    }
 }
 
 void displayMatrix(int **matrix, int n)
@@ -56,4 +70,40 @@ void displayMatrix(int **matrix, int n)
         printf("\n");
     }
     printf("\n");
+}
+
+void applySmoothing(int **matrix, int n)
+{
+    int *tempMatrix=(int*)malloc(n *n * sizeof(int));
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            *(tempMatrix + i * n + j) = *(*(matrix + i) + j);
+        }
+    }
+
+    for(int row = 0; row < n; row++)
+    {
+        for(int column = 0; column < n; column++)
+        {
+            int sum = 0;
+            int count = 0;
+
+            for(int i = row - 1; i <= row + 1; i++)
+            {
+                for(int j = column - 1; j <= column + 1; j++)
+                {
+                    if(i >= 0 && i < n && j >= 0 && j < n)
+                    {
+                        sum += *(tempMatrix + i * n + j);
+                        count++;
+                    }
+                }
+            }
+            *(*(matrix + row) + column) = sum / count; 
+        }
+    }
+    free(tempMatrix);
 }
