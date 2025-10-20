5ca8b070c05283600bd23e75a3700f9dc8698178
rishwanthk <iamrishwanthk@gmail.com>
2025-10-20 09:51:23 +0530

    added an input validation

--- imageProcessing.c
+++ imageProcessing.c
@@ -1,45 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void rotate90Degree(int **matrix, int n);
-void displayMatrix(int **matrix, int n);
-void applySmoothing(int **matrix, int n);
+void rotate90Degree(int **matrix, int matrixSize);
+void displayMatrix(int **matrix, int matrixSize);
+void applySmoothing(int **matrix, int matrixSize);
 
 int main()
 {
-    int n;
+    int matrixSize;
     printf("Enter the size of the matrix: \n");
-    scanf("%d",&n);
-
-    int **matrix = (int **)malloc(n * sizeof(int *));
-    for(int i = 0; i < n; i++)
+    scanf("%d",&matrixSize);
+    if(matrixSize < 2 || matrixSize > 10)
     {
-        *(matrix + i) = (int *)malloc(n * sizeof(int));
+        printf("Invalid size.\n");
+        return 1;
     }
 
-    for(int i = 0; i < n; i++)
+    int **matrix = (int **)malloc(matrixSize * sizeof(int *));
+    for(int i = 0; i < matrixSize; i++)
     {
-        for(int j = 0; j < n; j++)
+        *(matrix + i) = (int *)malloc(matrixSize * sizeof(int));
+    }
+
+    for(int i = 0; i < matrixSize; i++)
+    {
+        for(int j = 0; j < matrixSize; j++)
         {
             *(*(matrix + i) + j) = rand() % 256;
         }
     }
 
-    displayMatrix(matrix, n);
-    rotate90Degree(matrix, n);
-    displayMatrix(matrix, n);
-    applySmoothing(matrix, n);
-    displayMatrix(matrix, n);
+    displayMatrix(matrix, matrixSize);
+    rotate90Degree(matrix, matrixSize);
+    displayMatrix(matrix, matrixSize);
+    applySmoothing(matrix, matrixSize);
+    displayMatrix(matrix, matrixSize);
 
 
 }
 
 
-void rotate90Degree(int **matrix, int n)
+void rotate90Degree(int **matrix, int matrixSize)
 {
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < matrixSize; i++)
     {
-        for(int j = i; j < n; j++)
+        for(int j = i; j < matrixSize; j++)
         {
             int temp = *(*(matrix + i) + j);
             *(*(matrix + i) + j) = *(*(matrix + j) + i);
@@ -48,22 +53,22 @@ void rotate90Degree(int **matrix, int n)
         }
     }
 
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < matrixSize; i++)
     {
-        for(int j = 0; j < n/2; j++)
+        for(int j = 0; j < matrixSize/2; j++)
         {
             int temp = *(*(matrix + i) + j);
-            *(*(matrix + i) + j) = *(*(matrix + i) + (n - 1 - j));
-            *(*(matrix + i) + (n - 1 - j)) = temp;
+            *(*(matrix + i) + j) = *(*(matrix + i) + (matrixSize - 1 - j));
+            *(*(matrix + i) + (matrixSize - 1 - j)) = temp;
         }
     }
 }
 
-void displayMatrix(int **matrix, int n)
+void displayMatrix(int **matrix, int matrixSize)
 {
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < matrixSize; i++)
     {
-        for(int j = 0; j < n; j++)
+        for(int j = 0; j < matrixSize; j++)
         {
             printf("%d ", *(*(matrix + i) + j));
         }
@@ -72,21 +77,21 @@ void displayMatrix(int **matrix, int n)
     printf("\n");
 }
 
-void applySmoothing(int **matrix, int n)
+void applySmoothing(int **matrix, int matrixSize)
 {
-    int *tempMatrix=(int*)malloc(n *n * sizeof(int));
+    int *tempMatrix=(int*)malloc(matrixSize * matrixSize * sizeof(int));
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < matrixSize; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < matrixSize; j++)
         {
-            *(tempMatrix + i * n + j) = *(*(matrix + i) + j);
+            *(tempMatrix + i * matrixSize + j) = *(*(matrix + i) + j);
         }
     }
 
-    for(int row = 0; row < n; row++)
+    for(int row = 0; row < matrixSize; row++)
     {
-        for(int column = 0; column < n; column++)
+        for(int column = 0; column < matrixSize; column++)
         {
             int sum = 0;
             int count = 0;
@@ -95,9 +100,9 @@ void applySmoothing(int **matrix, int n)
             {
                 for(int j = column - 1; j <= column + 1; j++)
                 {
-                    if(i >= 0 && i < n && j >= 0 && j < n)
+                    if(i >= 0 && i < matrixSize && j >= 0 && j < matrixSize)
                     {
-                        sum += *(tempMatrix + i * n + j);
+                        sum += *(tempMatrix + i * matrixSize + j);
                         count++;
                     }
                 }
